8c6dc658bfb1825d206a6bea52a029646f4d9bc5
juntyr <juntyr@users.noreply.github.com>
2024-04-08 18:51:46 +0000

    deploy: ce9e5dbd555e72fa780ad1fe724b6ba33a45b3a1

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = new Map(JSON.parse('[\
-["pyodide_webassembly_runtime_layer",["",[],["conversion.rs","externref.rs","func.rs","global.rs","instance.rs","lib.rs","memory.rs","module.rs","store.rs","table.rs"]]]\
+["pyodide_webassembly_runtime_layer",["",[["features",[],["mod.rs"]]],["conversion.rs","externref.rs","func.rs","global.rs","instance.rs","lib.rs","memory.rs","module.rs","store.rs","table.rs"]]]\
 ]'));
 createSrcSidebar();
